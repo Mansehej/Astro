8c0d06e87369ff2ea0d942ed67b3a232ce423d23
Mansehej Singh <mansehej@gmail.com>
2020-10-13 19:17:04 +0530

    Create 'dateRange' onCall function
    Create https callable function that returns 10 items with selected fields, sorted by closest approach in the range of dates selected.

--- functions/index.js
+++ functions/index.js
@@ -26,10 +26,39 @@ exports.browse = functions.https.onCall(async (data, context) => {
 
         const listOfNeos = neoWsResponse.data.near_earth_objects
         const extractedList = listOfNeos.map(({id, name, estimated_diameter, is_potentially_hazardous_asteroid, close_approach_data}) => ({id, name, estimated_diameter, is_potentially_hazardous_asteroid, close_approach_data}))
-
         return extractedList
     }
     catch(error) {
         throw new functions.https.HttpsError('unknown', error.message)
     }
 });
+
+exports.dateRange = functions.https.onCall(async (data, context) => {
+
+    const startDate = data.startDate
+    const endDate = data.endDate
+
+    if(!API_KEY) {
+        throw new functions.https.HttpsError('unauthenticated', "No NeoWs API key provided.")
+    }
+
+    try {
+        const neoWsResponse = await axios.get(`${BASE_URL}/feed`, {
+            params: {
+                start_date: startDate,
+                end_date: endDate,
+                detailed: false,
+                api_key: API_KEY
+            }
+        })
+
+        const dateWiseNeos = neoWsResponse.data.near_earth_objects
+        const extractedList = Object.keys(dateWiseNeos).map(neos => dateWiseNeos[neos]).flat().map(({id, name, estimated_diameter, is_potentially_hazardous_asteroid, close_approach_data}) => ({id, name, estimated_diameter, is_potentially_hazardous_asteroid, close_approach_data}))
+        extractedList.sort((neo1, neo2) => neo1.close_approach_data[0].miss_distance.kilometers - neo2.close_approach_data[0].miss_distance.kilometers)
+        return extractedList.slice(0,10)
+    }
+    catch(error) {
+        throw new functions.https.HttpsError('unknown', error.message)
+    }
+    
+})
