7db6bc0f12524137112995e6855c95bb1b5be6da
Mansehej Singh <mansehej@gmail.com>
2020-10-23 12:50:01 +0530

    Add populate from date function to Browse page

--- src/pages/Browse.vue
+++ src/pages/Browse.vue
@@ -1,13 +1,13 @@
 <template>
   <q-page class="flex flex-center">
-    <div v-if="loaded" style="min-width: 30vw;">
+    <div v-if="loaded" style="min-width: 30vw">
       <q-toolbar class="bg-primary text-white shadow-2">
         <q-toolbar-title>Browse</q-toolbar-title>
       </q-toolbar>
       <asteroid-list :asteroidList="asteroidList" />
     </div>
     <div v-if="error" class="text-red text-bold text-center">
-        There was an error. Please try again later.
+      {{ errorMessage }}
     </div>
   </q-page>
 </template>
@@ -24,20 +24,70 @@ export default {
     return {
       loaded: false,
       asteroidList: [],
-      error: false
+      error: false,
+      errorMessage: "There was an error. Please try again later.",
     };
   },
   created() {
-    const vm = this;
-    const browseFunction = firebaseFunctions.httpsCallable("browse");
-    browseFunction({})
-      .then(function (result) {
-        vm.asteroidList = result.data;
-        vm.loaded = true;
-      })
-      .catch((error) => {
+    const query = this.$route.query;
+    if (!query.from || !query.to) {
+      this.populateFromBrowse();
+    } else {
+      this.populateFromDateRange(query.from, query.to);
+    }
+  },
+  methods: {
+    populateFromBrowse() {
+      const vm = this;
+      const browseFunction = firebaseFunctions.httpsCallable("browse");
+      browseFunction({})
+        .then(function (result) {
+          vm.asteroidList = result.data;
+          vm.loaded = true;
+        })
+        .catch((error) => {
+          this.error = true;
+          this.errorMessage = "There was an error. Please try again later.";
+        });
+    },
+    populateFromDateRange(startDate, endDate) {
+      const vm = this;
+      const dateFunction = firebaseFunctions.httpsCallable("dateRange");
+      const dateDifference = this.calculateDateDifference(startDate, endDate);
+      if (dateDifference > 7) {
+        this.errorMessage = "Dates cannot be more than 7 days apart.";
         this.error = true;
-      });
+        return;
+      }
+      dateFunction({ startDate, endDate })
+        .then(function (result) {
+          vm.asteroidList = result.data;
+          vm.loaded = true;
+        })
+        .catch((error) => {
+          this.error = true;
+          this.errorMessage = "There was an error. Please try again later.";
+        });
+    },
+    calculateDateDifference(startDate, endDate) {
+      const MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+      startDate = new Date(startDate);
+      endDate = new Date(endDate);
+
+      const utc1 = Date.UTC(
+        startDate.getFullYear(),
+        startDate.getMonth(),
+        startDate.getDate()
+      );
+      const utc2 = Date.UTC(
+        endDate.getFullYear(),
+        endDate.getMonth(),
+        endDate.getDate()
+      );
+
+      return Math.floor((utc2 - utc1) / MS_PER_DAY) + 1;
+    },
   },
 };
 </script>
