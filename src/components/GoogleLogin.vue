68c3fb4301b7f50e892f995211fd91bb3792f75a
Mansehej Singh <mansehej@gmail.com>
2020-10-23 14:03:18 +0530

    Create component to login via Google

---
+++ src/components/GoogleLogin.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-btn dense padding="none" class="q-mx-md" flat @click="googleLogin()">
+    <img src="../assets/googlelogin.png" />
+  </q-btn>
+</template>
+
+<script>
+import { firebaseAuth } from "boot/firebase";
+import * as firebase from "firebase/app";
+
+export default {
+  methods: {
+    googleLogin() {
+      // Using a popup.
+      var provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope("profile");
+      provider.addScope("email");
+      firebaseAuth.signInWithPopup(provider).catch((error) => {});
+    },
+  },
+};
+</script>
