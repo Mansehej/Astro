ea53d671051f7f94c31148b85866a2b9bdd69c42
Mansehej Singh <mansehej@gmail.com>
2020-10-24 23:38:10 +0530

    Create component for searching asteroid

---
+++ src/components/AsteroidSearch.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <q-input
+      standout="bg-teal text-white"
+      v-model="asteroidId"
+      label="Search Asteroid ID"
+      bottom-slots
+      :error="!!error"
+      :error-message="error"
+    >
+      <template v-slot:append>
+        <q-btn flat @click="searchAsteroid"
+          ><q-icon clickable name="search"
+        /></q-btn>
+      </template>
+    </q-input>
+
+    <q-dialog v-model="isResultGenerated">
+      <asteroid-card
+        v-if="isResultGenerated"
+        :name="asteroidInformation.name"
+        :id="asteroidInformation.id"
+        :estimatedDiameter="asteroidInformation.estimated_diameter"
+        :closeApproachData="asteroidInformation.close_approach_data"
+        :isPotentiallyHazardous="asteroidInformation.is_potentially_hazardous"
+      />
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+import { firebaseFunctions } from "boot/firebase";
+
+export default {
+  components: {
+    "asteroid-card": require("../components/AsteroidInformation.vue").default,
+  },
+  data() {
+    return {
+      asteroidId: "",
+      asteroidInformation: null,
+      isResultGenerated: false,
+      datePopup: false,
+      error: null,
+    };
+  },
+  methods: {
+    searchAsteroid() {
+      const vm = this;
+      if (this.asteroidId == "") {
+        this.error = "Please enter asteroid ID";
+        return;
+      }
+      var searchFunction = firebaseFunctions.httpsCallable("searchById");
+      searchFunction({ id: this.asteroidId })
+        .then(function (result) {
+          vm.asteroidInformation = result.data[0];
+          vm.isResultGenerated = true;
+        })
+        .catch((error) => {
+          if (error.code == "not-found") {
+            this.error = "Asteroid ID not found";
+          }
+        });
+    },
+  },
+};
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,20 +1,7 @@
 <template>
   <q-page class="flex flex-center">
     <div class="column">
-      <q-input
-        standout="bg-teal text-white"
-        v-model="asteroidId"
-        label="Search Asteroid ID"
-        bottom-slots
-        :error="!!error"
-        :error-message="error"
-      >
-        <template v-slot:append>
-          <q-btn flat @click="searchAsteroid"
-            ><q-icon clickable name="search"
-          /></q-btn>
-        </template>
-      </q-input>
+      <asteroid-search/>
       <div class="tex-subtitle text-center">OR</div>
       <div class="row justify-around q-mt-md">
         <q-btn @click="browseAsteroids(null)">Browse</q-btn>
@@ -22,18 +9,6 @@
       </div>
       <q-btn v-if="userDetails.uid" to="/favorites" class="q-mt-md">Favorites</q-btn>
     </div>
-
-    <q-dialog v-model="isResultGenerated">
-      <asteroid-card
-        v-if="isResultGenerated"
-        :name="asteroidInformation.name"
-        :id="asteroidInformation.id"
-        :estimatedDiameter="asteroidInformation.estimated_diameter"
-        :closeApproachData="asteroidInformation.close_approach_data"
-        :isPotentiallyHazardous="asteroidInformation.is_potentially_hazardous"
-      />
-    </q-dialog>
-
     <q-dialog v-model="datePopup">
       <date-range-picker @range-picked="browseAsteroids" />
     </q-dialog>
@@ -41,14 +16,13 @@
 </template>
 
 <script>
-import { firebaseFunctions } from "boot/firebase";
 import { mapState } from "vuex";
 
 
 export default {
   name: "PageIndex",
   components: {
-    "asteroid-card": require("../components/AsteroidInformation.vue").default,
+    "asteroid-search": require("../components/AsteroidSearch.vue").default,
     "date-range-picker": require("../components/DateRangePicker.vue").default,
   },
   data() {
@@ -64,24 +38,6 @@ export default {
     ...mapState("user", ["userDetails"]),
   },
   methods: {
-    searchAsteroid() {
-      const vm = this;
-      if (this.asteroidId == "") {
-        this.error = "Please enter asteroid ID";
-        return;
-      }
-      var searchFunction = firebaseFunctions.httpsCallable("searchById");
-      searchFunction({ id: this.asteroidId })
-        .then(function (result) {
-          vm.asteroidInformation = result.data[0];
-          vm.isResultGenerated = true;
-        })
-        .catch((error) => {
-          if (error.code == "not-found") {
-            this.error = "Asteroid ID not found";
-          }
-        });
-    },
     browseAsteroids(dateRange) {
       dateRange = dateRange ?? {};
       this.$router.push({ path: "browse", query: dateRange }).catch((err) => {
