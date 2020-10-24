b7e3275be9da9a8ee9dc23382daa800b056ae127
Mansehej Singh <mansehej@gmail.com>
2020-10-24 19:18:53 +0530

    Fix missing export in index of utils

--- functions/utils/index.js
+++ functions/utils/index.js
@@ -1,9 +1,10 @@
 const getFromNeoWs = require('./getFromNeoWs');
 const { extractFields, mergeNeoDates, sortByApproach } = require('./neoListParser')
-const { getFavoritesMap } = require('./userFavorites')
+const { getFavoritesMap, addFavorite } = require('./userFavorites')
 
 exports.getFromNeoWs = getFromNeoWs
 exports.extractFields = extractFields
 exports.mergeNeoDates = mergeNeoDates
 exports.sortByApproach = sortByApproach
 exports.getFavoritesMap = getFavoritesMap
+exports.addFavorite = addFavorite
